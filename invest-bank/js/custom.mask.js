f9e90889fa5c04dbd2013bf53eee5d92ce0178bc
Caio <midia@investbankcompany.com.br>
2021-10-13 14:36:14 -0300

    Alteração nos scripts de mascara

--- invest-bank/js/custom.mask.js
+++ invest-bank/js/custom.mask.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 
     $("#numero").mask("0#");
     $("#cep").mask("99999-999");
-    $(".data").mask("00/00/0000");
     $("#cpf").mask("999.999.999-99");
     $("#cel").mask('(99) 9 9999-9999').focusout(function(event) {
         var target, phone, element;
@@ -20,7 +19,26 @@ $(document).ready(function() {
     $("#nascimento").mask("00/00/0000");
     $("#cnpj").mask("00.000.000/0000-00");
     $("#renda").mask("#.##0,00", { reverse: true });
-    $(".dinheiro").mask("#.##0,00", { reverse: true });
-    $(".money").maskMoney({ prefix: "R$ ", decimal: ",", thousands: "." });
 
+    // Separar ID e CLASSE
+
+    $(".numero").mask("0#");
+    $(".cep").mask("99999-999");
+    $(".data").mask("00/00/0000");
+    $(".cpf").mask("999.999.999-99");
+    $(".cnpj").mask("00.000.000/0000-00");
+    $(".money").maskMoney({ prefix: "R$ ", decimal: ",", thousands: "." });
+    $(".tel").mask('(99) 9 9999-9999').focusout(function(event) {
+        var target, phone, element;
+        target = (event.currentTarget) ? event.currentTarget : event.srcElement;
+        phone = target.value.replace(/\D/g, '');
+        element = $(target);
+        element.unmask();
+
+        if (phone.length > 10) {
+            element.mask("(99) 9 9999-9999");
+        } else {
+            element.mask("(99) 9999-99999")
+        }
+    });
 });
