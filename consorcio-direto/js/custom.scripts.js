bfae60627a8d22be10157789ca7d6397e21a6d3b
Caio <midia@investbankcompany.com.br>
2022-01-31 15:25:33 -0300

    Update

--- consorcio-direto/js/custom.scripts.js
+++ consorcio-direto/js/custom.scripts.js
@@ -1,14 +1,14 @@
 // Script da barra de simulação (Range)
-var slider = document.getElementByClass("slider");
-var print = document.getElementByClass("print");
-print.innerHTML = slider.value;
-slider.oninput = function() {
+var rangeVal = document.getElementByClass("rangeVal");
+var printVal = document.getElementByClass("print");
+print.innerHTML = rangeVal.value;
+rangeVal.oninput = function() {
     print.innerHTML = parseInt(this.value).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
 }
 
 // Disparar o evento manualmente
 var event = new CustomEvent("input");
-slider.dispatchEvent(event);
+rangeVal.dispatchEvent(event);
 
 // Cookie
 /*
