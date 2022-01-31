60bae94d3b92592cd892c5ac62c4f0dcc9c0ca27
Caio <midia@investbankcompany.com.br>
2022-01-31 15:36:31 -0300

    Update

--- consorcio-direto/js/custom.scripts.js
+++ consorcio-direto/js/custom.scripts.js
@@ -1,11 +1,22 @@
-// Script da barra de simulação (Range)
-var rangeVal = document.getElementByClass("rangeVal");
-var printVal = document.getElementByClass("print");
-print.innerHTML = rangeVal.value;
-rangeVal.oninput = function() {
-    print.innerHTML = parseInt(this.value).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+// Puxar/converter valores
+var parcela = document.getElementsByClassName("parcela");
+var credito = document.getElementsByClassName("credito");
+var printParcela = document.getElementsByClassName("printParcela");
+var printCredito = document.getElementsByClassName("printCredito");
+
+printParcela.innerHTML = parcela.value;
+printCredito.innerHTML = credito.value;
+
+parcela.oninput = function() {
+    printParcela.innerHTML = parseInt(this.value).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+}
+
+credito.oninput = function() {
+    printCredito.innerHTML = parseInt(this.value).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
 }
 
 // Disparar o evento manualmente
 var event = new CustomEvent("input");
-rangeVal.dispatchEvent(event);
+var event = new CustomEvent("input");
+parcela.dispatchEvent(event);
+credito.dispatchEvent(event);
