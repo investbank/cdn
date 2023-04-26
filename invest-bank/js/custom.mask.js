1face66ebad78526f08628129ec0b1158e6aa3c5
investbank <51759829+investbank@users.noreply.github.com>
2023-04-25 21:07:56 -0300

    Atualização no RG parte 2

--- invest-bank/js/custom.mask.js
+++ invest-bank/js/custom.mask.js
@@ -1,38 +1,38 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    // Block filling of letters in forms, allowing only numbers
-    $(".nums").mask("0#");
+	// Block filling of letters in forms, allowing only numbers
+	$(".nums").mask("0#");
 
-    // Mask for filling dates
-    $(".dates").mask("00/00/0000");
+	// Mask for filling dates
+	$(".dates").mask("00/00/0000");
 
-    // Mask for filling Zip Codes
-    $(".zip-codes").mask("00000-000");
+	// Mask for filling Zip Codes
+	$(".zip-codes").mask("00000-000");
 
-    // Mask for filling phone/cell phone
-    $(".phone").mask("(00) 0 0000-0000").focusout(function(event) {
-        var target, phone, element;
-        target = (event.currentTarget) ? event.currentTarget : event.srcElement;
-        phone = target.value.replace(/\D/g, "");
-        element = $(target);
-        element.unmask();
+	// Mask for filling phone/cell phone
+	$(".phone").mask("(00) 0 0000-0000").focusout(function (event) {
+		var target, phone, element;
+		target = (event.currentTarget) ? event.currentTarget : event.srcElement;
+		phone = target.value.replace(/\D/g, "");
+		element = $(target);
+		element.unmask();
 
-        if (phone.length > 10) {
-            element.mask("(00) 0 0000-0000");
-        } else {
-            element.mask("(00) 0000-00000")
-        }
-    });
+		if (phone.length > 10) {
+			element.mask("(00) 0 0000-0000");
+		} else {
+			element.mask("(00) 0000-00000")
+		}
+	});
 
-    // Mask for filling email
-    $(".email").mask("A", {
-        translation: {
-            "A": { pattern: /[\w@\-.+]/, recursive: true }
-        }
-    });
+	// Mask for filling email
+	$(".email").mask("A", {
+		translation: {
+			"A": { pattern: /[\w@\-.+]/, recursive: true }
+		}
+	});
 
-    // Mask for filling RG
-	$('.rg').mask('00.000.000-X', {
+	// Mask for filling RG
+	$('#rg').mask('00.000.000-X', {
 		translation: {
 			'X': {
 				pattern: /[xX\d]/,
@@ -42,6 +42,7 @@ $(document).ready(function() {
 		onKeyPress: function (rg, event, currentField, options) {
 			if (!validarRG(rg)) {
 				currentField.addClass('error');
+				alert('RG inválido!');
 			} else {
 				currentField.removeClass('error');
 			}
@@ -78,14 +79,14 @@ $(document).ready(function() {
 
 		return dv === dvCalculado;
 	}
-    
-    // Mask for filling CPF
-    $(".cpf").mask("000.000.000-00");
 
-    // Mask for filling CNPJ
-    $(".cnpj").mask("00.000.000/0000-00");
+	// Mask for filling CPF
+	$(".cpf").mask("000.000.000-00");
 
-    // Mask for filling money (with R$)
-    $(".money").maskMoney({ prefix: "R$ ", decimal: ",", thousands: "." });
+	// Mask for filling CNPJ
+	$(".cnpj").mask("00.000.000/0000-00");
+
+	// Mask for filling money (with R$)
+	$(".money").maskMoney({ prefix: "R$ ", decimal: ",", thousands: "." });
 
 });
